109def0c4ac3f19cf970a22db9f2270a53ff61e5
Elinaldo Bezerra <elinaldo_bezerra@hotmail.com>
2021-05-20 19:50:18 -0300

    Added my project

---
+++ src/test/java/com/educa/educaapi/EducaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educa.educaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
